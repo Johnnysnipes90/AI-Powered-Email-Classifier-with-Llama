50fa681e424feeabb5d54e87ca632b011632de43
John Olalemi <johnnysnipes90@gmail.com>
2025-07-21 05:21:41 +0100

    streamlit

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-ECHO is on.
+import streamlit as st
+from src.classifier import classify_email
+from src.llama_initializer import load_llama_model
+
+st.title("📧 AI-Powered Email Classifier")
+
+email_text = st.text_area("Enter Email Content")
+if st.button("Classify"):
+    model = load_llama_model("model/model.gguf")
+    category = classify_email(model, email_text)
+    st.success(f"🧠 Predicted Category: {category}")

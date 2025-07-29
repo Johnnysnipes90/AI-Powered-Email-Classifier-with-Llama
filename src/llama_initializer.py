9dc88c5becbf4e42418c91be53eaed3f9592f9ab
John Olalemi <johnnysnipes90@gmail.com>
2025-07-29 11:20:26 +0100

    updates llama initializer

--- src/llama_initializer.py
+++ src/llama_initializer.py
@@ -1,4 +1,8 @@
 from llama_cpp import Llama
 
-def load_llama_model(path_to_model, ctx=512, threads=4):
-    return Llama(model_path=path_to_model, n_ctx=ctx, n_threads=threads)
+def load_llama_model(model_path: str, context_length: int = 1024):
+    return Llama(
+        model_path=model_path,
+        n_ctx=context_length,
+        verbose=False
+    )
